9c80a9db7a1a1b611e1f6572667339dd1f218fcf
heebrahymne <bankoleheebrahymn@gmail.com>
2025-01-21 10:21:59 +0100

    Updating the coyright in the footer section

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = ({isDarkMode}) => {
             </div>
         </div>
           <div className='text-center sm:flex items-center justify-between border-t border-gra-400 mx-[10%] mt-12 py-6'>
-            <p>@2024 Ayodele Bankole. All rights reserved.</p>
+            <p>@2025 Ayodele Bankole. All rights reserved.</p>
             <ul className='flex items-center justify-center gap-10 mt-4 sm:mt-0 '>
                 <li><a target='_blank' href='#'>Github</a></li>
                 <li><a target='_blank' href='#'>Linkedln</a></li>
